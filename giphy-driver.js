4b99e2195c20216e4d252e7f5b87c08add0864d1
gjiki <jikia.gegi@gmail.com>
2020-04-27 01:43:59 +0400

    Responcive part left
    Searchs finished, localstorage fixed, added gif ratings. Only responcive part left.

--- giphy-driver.js
+++ giphy-driver.js
@@ -1,14 +1,16 @@
-import * as CONFIG from "./config.js";
-
 export class GiphyDriver {
     constructor(url, key) {
         this._url = url;
         this._key = key;
     }
 
-    getGifs() {
-        const searchValue = document.getElementById("search-input").value;
-        const giphyAPI = CONFIG.URL + "q=" + searchValue + "&api_key=" + CONFIG.KEY;
+    getGifs(searchOrTrend, searchValue) {
+        let giphyAPI;
+        if (searchOrTrend) {
+            giphyAPI = this._url + "q=" + searchValue + "&api_key=" + this._key;
+        } else {
+            giphyAPI = this._url + "&api_key=" + this._key;
+        }
 
         fetch(giphyAPI)
             .then(response => {
@@ -17,15 +19,20 @@ export class GiphyDriver {
             .then(json => {
                 let gifsDiv = document.getElementById("gifs-div");
                 for (let i = 0; i < json.data.length; i++) {
-                    let gifDiv = document.createElement("div");
-                    gifDiv.setAttribute("class", "gif-div-item");
+                    const curUrl = json.data[i].images.original.url;
+                    const width = json.data[i].images.fixed_height_small_still.width;
 
-                    gifsDiv.appendChild(gifDiv);
-                    let curUrl = json.data[i].images.original.url;
-                    let width = json.data[i].images.fixed_height_small_still.width;
+                    let gifDivItem = document.createElement("div");
+                    gifDivItem.setAttribute("class", "gif-div-item");
 
-                    gifDiv.innerHTML = '<img src="' + curUrl + '">';
-                    gifDiv.style = "width:" + width + ";";
+                    let imgDiv = document.createElement("div");
+                    imgDiv.innerHTML = "Rating: " + json.data[i].rating;
+                    imgDiv.setAttribute('class', 'rating');
+
+                    gifDivItem.innerHTML = '<img src="' + curUrl + '">';
+                    gifDivItem.appendChild(imgDiv);
+                    gifDivItem.style = "width:" + width + ";";
+                    gifsDiv.appendChild(gifDivItem);
                 }
             })
             .catch(err => console.log(err));
--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-const URL = "https://api.giphy.com/v1/gifs/search?";
-const TRENDING_URL = "https://api.giphy.com/v1/gifs/trending?";
-const KEY = "aFFKTuSMjd6j0wwjpFCPXZipQbcnw3vB";
-const DEFAULT_SEARCHS = ["Internet Cats", "Meme's", "Typing", "Space", "Rick and Morty"];
+import * as CONFIG from "./config.js";
+import { GiphyDriver } from "./giphy-driver.js";
 
 function loadSearchs(history) {
     if (history != null) {
@@ -13,8 +11,15 @@ function loadSearchs(history) {
 }
 
 function addButton(searchValue) {
-    addSearch(searchValue);
-    addToStorage(searchValue);
+    let arr = JSON.parse(localStorage.getItem('history'));
+    if (arr == null) arr = [];
+    arr = Array.from(arr);
+    let ind = arr.indexOf(searchValue);
+
+    if (ind < 0) {
+        addSearch(searchValue);
+        addToStorage(searchValue);
+    }
 }
 
 function xButton(value, id) {
@@ -32,10 +37,14 @@ function addSearch(searchValue) {
     let btn = document.createElement("button");
     btn.innerHTML = searchValue;
     btn.setAttribute("class", "btn");
+    btn.addEventListener("click", function() {
+        searchButtonClick(searchValue);
+    }, false);
+
     btn.setAttribute("id", searchValue);
     div.appendChild(btn);
 
-    let ind = DEFAULT_SEARCHS.indexOf(searchValue);
+    let ind = CONFIG.DEFAULT_SEARCHS.indexOf(searchValue);
     if (ind < 0) {
         let x_btn = document.createElement("button");
         x_btn.setAttribute("class", "btn delete-button");
@@ -50,19 +59,12 @@ function addSearch(searchValue) {
 
 function removeSearch(id) {
     let elem = document.getElementById(id);
-    if (elem != null) {
-        if (elem.parentNode != null) {
-            elem.parentNode.removeChild(elem);
-        }
-    }
+    elem.parentNode.removeChild(elem);
 }
 
 function addToStorage(searchValue) {
-    let arr = null;
-    if (JSON.parse(localStorage.getItem('history')) == null) {
-        arr = [];
-    } else arr = JSON.parse(localStorage.getItem('history'));
-
+    let arr = JSON.parse(localStorage.getItem('history'));
+    if (arr == null) arr = [];
     arr = Array.from(arr);
     arr.push(searchValue);
     localStorage.setItem('history', JSON.stringify(arr));
@@ -70,73 +72,60 @@ function addToStorage(searchValue) {
 
 function removeFromStorage(searchValue) {
     let arr = JSON.parse(localStorage.getItem('history'));
-    if (arr != null) {
-        arr = Array.from(arr);
-        let ind = arr.indexOf(searchValue);
-        arr.splice(ind, 1);
-        localStorage.setItem('history', JSON.stringify(arr));
-    }
+    if (arr == null) arr = [];
+    arr = Array.from(arr);
+    let ind = arr.indexOf(searchValue);
+    arr.splice(ind, 1);
+    localStorage.setItem('history', JSON.stringify(arr));
 }
 
 function loadHistory() {
     let history_div = document.getElementById("history-div");
     history_div.innerHTML = "";
 
-    let defaults = DEFAULT_SEARCHS;
+    let defaults = CONFIG.DEFAULT_SEARCHS;
     let history = JSON.parse(localStorage.getItem('history'));
+    if (history == null) history = [];
 
-    if (history != null) {
-        history = Array.from(history);
-        let full = defaults.concat(history);
-        loadSearchs(full);
-    } else {
-        loadSearchs(defaults);
-    }
+    history = Array.from(history);
+    let full = defaults.concat(history);
+    loadSearchs(full);
+
+    let searchInputValue = document.getElementById("submit-btn");
+    searchInputValue.addEventListener("click", function() {
+        searchInput();
+    }, false);
+
+    let searchTrend = document.getElementById("trend-btn");
+    searchTrend.addEventListener("click", function() {
+        searchTrends();
+    }, false);
 }
 
-function search(value) {
+function searchInput() {
     let gifs_div = document.getElementById("gifs-div");
     gifs_div.innerHTML = "";
-    if (value == "search") {
-        let searchValue = document.getElementById("search-input").value;
-        addButton(searchValue);
-        getGifs(true);
-    } else {
-        getGifs(false);
-    }
+    const searchValue = document.getElementById("search-input").value;
+    addButton(searchValue);
+    let g = new GiphyDriver(CONFIG.URL, CONFIG.KEY);
+    g.getGifs(true, searchValue);
+}
+
+function searchTrends() {
+    let gifs_div = document.getElementById("gifs-div");
+    gifs_div.innerHTML = "";
+    let g = new GiphyDriver(CONFIG.TRENDING_URL, CONFIG.KEY);
+    g.getGifs(false, " ");
+}
+
+function searchButtonClick(searchValue) {
+    let gifs_div = document.getElementById("gifs-div");
+    gifs_div.innerHTML = "";
+    let g = new GiphyDriver(CONFIG.URL, CONFIG.KEY);
+    g.getGifs(true, searchValue);
 }
 
 window.onload = function() {
     // For debugging : localStorage.clear();
     loadHistory();
-};
-
-function getGifs(value) {
-    let giphyAPI = "";
-    if (value == true) {
-        const searchValue = document.getElementById("search-input").value;
-        giphyAPI = URL + "q=" + searchValue + "&api_key=" + KEY;
-    } else {
-        giphyAPI = TRENDING_URL + "&api_key=" + KEY;
-    }
-
-    fetch(giphyAPI)
-        .then(response => {
-            return response.json();
-        })
-        .then(json => {
-            let gifsDiv = document.getElementById("gifs-div");
-            for (let i = 0; i < json.data.length; i++) {
-                let gifDiv = document.createElement("div");
-                gifDiv.setAttribute("class", "gif-div-item");
-
-                gifsDiv.appendChild(gifDiv);
-                let curUrl = json.data[i].images.original.url;
-                let width = json.data[i].images.fixed_height_small_still.width;
-
-                gifDiv.innerHTML = '<img src="' + curUrl + '">';
-                gifDiv.style = "width:" + width + ";";
-            }
-        })
-        .catch(err => console.log(err));
 }
